6f840d45afccbe6ce2b57e827d841d8a7aff46ea
Haim Guel Quiroz <hgq_2000@hotmail.com>
2022-04-22 13:33:31 -0500

    Add launchx route

--- app.js
+++ app.js
@@ -9,6 +9,11 @@ app.get("/", (request, response) => {
   response.send("Hola a todos los explorers");
 });
 
+//localhost:3000/launchx
+app.get("/launchx", (request, response) => {
+  response.send("Bienvenidos a launchX");
+});
+
 app.listen(port, () => {
   console.log("Servidor listo");
 });
